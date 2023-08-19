288530b1a598e8828db761a0101956933e784e45
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-08-19 13:46:45 +0500

    make some UI

---
+++ app.js
@@ -0,0 +1,18 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDRdwI7MsBM7-ktK4A45FduIptdNMXYnak",
+    authDomain: "hackathon-4aceb.firebaseapp.com",
+    projectId: "hackathon-4aceb",
+    storageBucket: "hackathon-4aceb.appspot.com",
+    messagingSenderId: "483540791511",
+    appId: "1:483540791511:web:2ad5a43a848af8e67f0092"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+
+
+
+  
+
